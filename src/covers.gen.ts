2c8c7cba9335afc715023822163bc7f66daa5093
Yanzhen Yu <yanzhen@smartx.com>
2024-03-02 21:44:15 +0800

    update data

--- src/covers.gen.ts
+++ src/covers.gen.ts
@@ -110,5 +110,6 @@ export const covers: Record<string, any> = {
   ['cover_685']: require('../assets/covers/685.png'),
   ['cover_686']: require('../assets/covers/686.png'),
   ['cover_687']: require('../assets/covers/687.png'),
-  ['cover_688']: require('../assets/covers/688.png')
+  ['cover_688']: require('../assets/covers/688.png'),
+  ['cover_689']: require('../assets/covers/689.png')
 }
